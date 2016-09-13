2716d8d08289111f35436dc1bdde3828e896da56
Grzegorz Kwaśniewski <grzegrz.kwasniewski2@gmail.com>
2016-09-13 13:35:42 +0200

    Add validation for title, author and tracks count fields

--- server.js
+++ server.js
@@ -50,20 +50,44 @@ app.get('/musicalbums/:id', function (req, res) {
 
 app.post('/musicalbums', function (req, res) {
     
-    var regex = new RegExp('[0-9]{4}-[0-9]{2}-[0-9]{2}')
+//       title: 'Nowe Światło',
+//    author: 'Mioush Onar',
+//    trackscount: 12,
+//    publisher: 'Step Records',
+//    publishedDate: '2014-10-01',
+//    ownLimitedEdition: true,
+//    ownPhysicalCD: true,
+//    ownDigital: false
+
+    var regexForDate = new RegExp('^(19|20)[0-9][0-9]-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
+    var regexForTracksCount = new RegExp('^([0-9]|[0-9][0-9])$')
     
     // filter JSON fields from body of the message
-    var body = _.pick(req.body, 'publishedDate');
+    var body = _.pick(req.body, 'title', 'author', 'trackscount', 'publishedDate')
     var publishedDateFromString = body.publishedDate
     
-    if (!regex.test(publishedDateFromString) || _.isEmpty(body.publishedDate)) {
-        res.status(400).send('Published date must have valid format \(yyyy-mm-dd\) and can\'t be null')
-    } else {
-        //body.title = body.title.trim();    
-        body.id = musicAlbumNextID++;
-        musicAlbums.push(body);
-        res.json(musicAlbums);
+    if (!_.isString(body.title) || body.title.trim().length === 0) {
+        res.status(400).send('Title field for music album must be in string format and can\'t be null or empty')
+    }
+    
+    if (!_.isString(body.author) || body.author.trim().length === 0) {
+        res.status(400).send('Author field for music album must be in string format and can\'t be null or empty')
+    }
+    
+    if (!_.isNumber(body.trackscount) || !regexForTracksCount.test(body.trackscount)) {
+        res.status(400).send('Number of tracks field for music album must be in int format (from 0 to 99) and can\'t be null or empty')
+        console.log('bad bad bad')
+    }
+    
+    // TODO try to change this to one function with bool return
+    if (!regexForDate.test(publishedDateFromString) || _.isEmpty(body.publishedDate)) {
+        res.status(400).send('Published date must have valid format \(yyyy-mm-dd\), must be in range of 1900-01-01 - 2099-12-31 and can\'t be null or empty')
     } 
+    
+    body.id = musicAlbumNextID++;
+    musicAlbums.push(body);
+    res.json(musicAlbums);
+    
 })
 
 app.listen(PORT, function () {
