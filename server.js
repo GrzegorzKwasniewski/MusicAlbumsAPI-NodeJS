b0fb674a7c2cefe08e2ed3668c4de7a7b711dad5
Grzegorz Kwaśniewski <grzegrz.kwasniewski2@gmail.com>
2016-09-14 13:48:56 +0200

    Add validation for publisher, ownLimitedEdition, ownPhysicalCD, ownDigital fields

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/musicalbums', function (req, res) {
     
 //       title: 'Nowe Światło',
 //    author: 'Mioush Onar',
-//    trackscount: 12,
+//    tracksCount: 12,
 //    publisher: 'Step Records',
 //    publishedDate: '2014-10-01',
 //    ownLimitedEdition: true,
@@ -63,7 +63,7 @@ app.post('/musicalbums', function (req, res) {
     var regexForTracksCount = new RegExp('^([0-9]|[0-9][0-9])$')
     
     // filter JSON fields from body of the message
-    var body = _.pick(req.body, 'title', 'author', 'trackscount', 'publishedDate')
+    var body = _.pick(req.body, 'title', 'author', 'tracksCount', 'publisher',  'publishedDate', 'ownLimitedEdition', 'ownPhysicalCD', 'ownDigital')
     var publishedDateFromString = body.publishedDate
     
     if (!_.isString(body.title) || body.title.trim().length === 0) {
@@ -74,15 +74,30 @@ app.post('/musicalbums', function (req, res) {
         res.status(400).send('Author field for music album must be in string format and can\'t be null or empty')
     }
     
-    if (!_.isNumber(body.trackscount) || !regexForTracksCount.test(body.trackscount)) {
+    if (!_.isNumber(body.tracksCount) || !regexForTracksCount.test(body.tracksCount)) {
         res.status(400).send('Number of tracks field for music album must be in int format (from 0 to 99) and can\'t be null or empty')
-        console.log('bad bad bad')
+    }
+    
+    if (!_.isString(body.publisher) || body.publisher.trim().length === 0) {
+        res.status(400).send('Publisher field for music album must be in string format and can\'t be null or empty')
     }
     
     // TODO try to change this to one function with bool return
     if (!regexForDate.test(publishedDateFromString) || _.isEmpty(body.publishedDate)) {
         res.status(400).send('Published date must have valid format \(yyyy-mm-dd\), must be in range of 1900-01-01 - 2099-12-31 and can\'t be null or empty')
-    } 
+    }
+    
+    if (!_.isBoolean(body.ownLimitedEdition)) {
+        res.status(400).send('Own Limited Edition field for music album must be in boolean format and can\'t be null')
+    }
+    
+    if (!_.isBoolean(body.ownPhysicalCD)) {
+        res.status(400).send('Own Physical CD field for music album must be in boolean format and can\'t be null')
+    }
+    
+    if (!_.isBoolean(body.ownDigital)) {
+        res.status(400).send('Own Digital field for music album must be in boolean format and can\'t be null')
+    }
     
     body.id = musicAlbumNextID++;
     musicAlbums.push(body);
