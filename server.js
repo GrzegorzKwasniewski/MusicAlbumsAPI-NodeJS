16d5f241dcd65aa0c0770d58143fe7adc93ece63
Grzegorz Kwaśniewski <grzegrz.kwasniewski2@gmail.com>
2016-09-15 14:25:22 +0200

    Add PUT method

--- server.js
+++ server.js
@@ -69,36 +69,36 @@ app.post('/musicalbums', function (req, res) {
     var publishedDateFromString = body.publishedDate
     
     if (!_.isString(body.title) || body.title.trim().length === 0) {
-        res.status(400).send('Title field for music album must be in string format and can\'t be null or empty')
+        return res.status(400).send('Title field for music album must be in string format and can\'t be null or empty')
     }
     
     if (!_.isString(body.author) || body.author.trim().length === 0) {
-        res.status(400).send('Author field for music album must be in string format and can\'t be null or empty')
+        return res.status(400).send('Author field for music album must be in string format and can\'t be null or empty')
     }
     
     if (!_.isNumber(body.tracksCount) || !regexForTracksCount.test(body.tracksCount)) {
-        res.status(400).send('Number of tracks field for music album must be in int format (from 0 to 99) and can\'t be null or empty')
+        return res.status(400).send('Number of tracks field for music album must be in int format (from 0 to 99) and can\'t be null or empty')
     }
     
     if (!_.isString(body.publisher) || body.publisher.trim().length === 0) {
-        res.status(400).send('Publisher field for music album must be in string format and can\'t be null or empty')
+        return res.status(400).send('Publisher field for music album must be in string format and can\'t be null or empty')
     }
     
     // TODO try to change this to one function with bool return
     if (!regexForDate.test(publishedDateFromString) || _.isEmpty(body.publishedDate)) {
-        res.status(400).send('Published date must have valid format \(yyyy-mm-dd\), must be in range of 1900-01-01 - 2099-12-31 and can\'t be null or empty')
+        return res.status(400).send('Published date must have valid format \(yyyy-mm-dd\), must be in range of 1900-01-01 - 2099-12-31 and can\'t be null or empty')
     }
     
     if (!_.isBoolean(body.ownLimitedEdition)) {
-        res.status(400).send('Own Limited Edition field for music album must be in boolean format and can\'t be null')
+        return res.status(400).send('Own Limited Edition field for music album must be in boolean format and can\'t be null')
     }
     
     if (!_.isBoolean(body.ownPhysicalCD)) {
-        res.status(400).send('Own Physical CD field for music album must be in boolean format and can\'t be null')
+        return res.status(400).send('Own Physical CD field for music album must be in boolean format and can\'t be null')
     }
     
     if (!_.isBoolean(body.ownDigital)) {
-        res.status(400).send('Own Digital field for music album must be in boolean format and can\'t be null')
+        return res.status(400).send('Own Digital field for music album must be in boolean format and can\'t be null')
     } else {
         body.id = musicAlbumNextID++;
         musicAlbums.push(body);
@@ -116,6 +116,64 @@ app.delete('/musicalbums/:id', function (req, res) {
         musicAlbums = _.without(musicAlbums, matchedMusicAlbum);
         res.json(matchedMusicAlbum);
     }
+})
+
+app.put('/musicalbums/:id', function (req, res) {
+    
+    var regexForDate = new RegExp('^(19|20)[0-9][0-9]-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$')
+    var regexForTracksCount = new RegExp('^([0-9]|[0-9][0-9])$')
+    
+    var musicAlbumId = parseInt(req.params.id, 10)
+    var matchedMusicAlbum = _.findWhere(musicAlbums, {id: musicAlbumId})
+    var body = _.pick(req.body, 'title', 'author', 'tracksCount', 'publisher',  'publishedDate', 'ownLimitedEdition', 'ownPhysicalCD', 'ownDigital')
+    var publishedDateFromString = body.publishedDate
+    var validAttributes = {}
+        
+    if (!matchedMusicAlbum) {
+        return res.status(404).json({"error": "No music album found with that id"});
+    }
+    
+    if (body.hasOwnProperty('title') && _.isString(body.title) && body.title.trim().length > 0) {
+        validAttributes.title = body.title
+    } else if (body.hasOwnProperty('title')) {
+        return res.status(400).json({"error": 'Title field for music album must be in string format and can\'t be null or empty'})
+    }
+    
+    if (body.hasOwnProperty('author') && _.isString(body.author) && body.author.trim().length > 0) {
+        validAttributes.author = body.author
+    } else if (body.hasOwnProperty('author')) {
+        return res.status(400).json({"error": 'Author field for music album must be in string format and can\'t be null or empty'})
+    }
+    
+    if (body.hasOwnProperty('tracksCount') && _.isNumber(body.tracksCount) && regexForTracksCount.test(body.tracksCount)) {
+        validAttributes.tracksCount = body.tracksCount
+    } else if (body.hasOwnProperty('tracksCount')) {
+        return res.status(400).json({"error": 'Number of tracks field for music album must be in int format (from 0 to 99) and can\'t be null or empty'})
+    }
+    
+    if (body.hasOwnProperty('publisher') && _.isString(body.publisher) && body.publisher.trim().length > 0) {
+        validAttributes.publisher = body.publisher
+    } else if (body.hasOwnProperty('publisher')) {
+        return res.status(400).json({"error": 'Publisher field for music album must be in string format and can\'t be null or empty'})
+    }
+    
+    if (body.hasOwnProperty('publishedDate') && regexForDate.test(publishedDateFromString) && !_.isEmpty(body.publishedDate)) {
+        validAttributes.publishedDate = body.publishedDate
+    } else if (body.hasOwnProperty('publishedDate')) {
+        return res.status(400).json({"error": 'Published date must have valid format \(yyyy-mm-dd\), must be in range of 1900-01-01 - 2099-12-31 and can\'t be null or empty'})
+    }
+                                    
+    if (body.hasOwnProperty('ownLimitedEdition') && _.isBoolean(body.ownLimitedEdition)) {
+        validAttributes.ownLimitedEdition = body.ownLimitedEdition
+    } else if (body.hasOwnProperty('ownLimitedEdition')) {
+        return res.status(400).json({"error": 'Own Limited Edition field for music album must be in boolean format and can\'t be null'})
+    }
+        
+    // update musicsAlbums array because object under matchedMusicAlbum is passed by reference
+    _.extend(matchedMusicAlbum, validAttributes)
+    
+    res.json(matchedMusicAlbum)
+    
 });
 
 app.listen(PORT, function () {
